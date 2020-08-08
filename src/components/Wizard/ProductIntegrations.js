9d07c7cc82adf5f63fa950fa4dbfb14a0139f26b
Mauricio Aspiazo <mauricioaspiazo@Mauricios-MacBook-Pro.local>
2020-08-08 17:33:10 -0500

    Cards

---
+++ src/components/Wizard/ProductIntegrations.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Card, CardDeck } from 'react-bootstrap';
+
+export const ProductIntegrations = ({ siteSelectedCallback }) => (
+  <>
+    <CardDeck>
+      <Card>
+        <Card.Img variant="top" src="http://placehold.it/50x50" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="http://placehold.it/50x50" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="http://placehold.it/50x50" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="http://placehold.it/50x50" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="http://placehold.it/50x50" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardDeck>
+  </>
+);
