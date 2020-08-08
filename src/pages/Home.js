75273e5b6e54afc9399ea830278b72b4ee533796
Mauricio Aspiazo <mauricioaspiazo@Mauricios-MacBook-Pro.local>
2020-08-08 14:47:38 -0500

    Content tweaks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,27 +3,38 @@ import { Content_50 } from '../components/content_50';
 import { Hero } from '../components/Hero';
 import { Container, Row } from 'react-bootstrap';
 
-const firstContentProps = {
-    imgUrl: "http://placehold.it/300x300",
-    title: "Lorem Ipsum",
-    headline: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    ctaUrl: "http://www.google.com",
-    ctaText: "Lorem Ipsum"
-}
+const complianceContentProps = {
+  imgUrl: 'http://placehold.it/300x300',
+  title: 'Compliance',
+  headline:
+    "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  ctaUrl: 'http://www.google.com',
+  ctaText: 'Lorem Ipsum',
+};
 
-export const Home = () =>
-    <Container fluid className="mb-5">
-        <Row className="mt-5">
-            <Hero
-                headline="Welcome to Tauruseer"
-                copy="Tauruseer's lightweight continuous assurance platform automates cognitive risk through existing code pipelines and cloud solutions to proactively aggregate siloed data, identify risk, and recommend the right actions to protect against preventable loss caused by human, process, and product threats. "
-                ctaCopy="Get Started"
-            />
-        </Row>
-        <Row className="mt-5">
-            <Content_50 {...firstContentProps} />
-        </Row>
-        <Row className="mt-5 row-reverse">
-            <Content_50 {...firstContentProps} />
-        </Row>
-    </Container>
+const securityContentProps = {
+  imgUrl: 'http://placehold.it/300x300',
+  title: 'Security',
+  headline:
+    "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  ctaUrl: 'http://www.google.com',
+  ctaText: 'Lorem Ipsum',
+};
+
+export const Home = () => (
+  <Container fluid className="mb-5">
+    <Row className="mt-5">
+      <Hero
+        headline="Your Continuous Assurance Platform"
+        copy="Tauruseer's lightweight continuous assurance platform automates cognitive risk through existing code pipelines and cloud solutions to proactively aggregate siloed data, identify risk, and recommend the right actions to protect against preventable loss caused by human, process, and product threats. "
+        ctaCopy="Get Started"
+      />
+    </Row>
+    <Row className="mt-5">
+      <Content_50 {...securityContentProps} />
+    </Row>
+    <Row className="mt-5 row-reverse">
+      <Content_50 {...complianceContentProps} />
+    </Row>
+  </Container>
+);
