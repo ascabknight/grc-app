5d9285f7fc2b140d13fe5de3066cfc080acee70a
Steven Gonzalez <stevengonzalez.globant@latam.com>
2020-08-08 10:42:44 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import React from 'react';
-import { render } from 'react-dom';
-import Home from './pages/Home';
+import ReactDOM from 'react-dom';
+import { Home } from './pages/Home';
 
-const App = () => {
-  return <Home />;
-};
+// Set up pages using the React Router Link element for navigation - instead of <a></a>
+const App = () => (
+  <main>
+    <h1>React Router</h1>
 
-render(React.createElement(App), document.getElementById('root'));
+    <div>
+      <Route exact path='/' component={Home} />
+    </div>
+  </main>
+)
+
+
+//Render app into the root HTML DOM node
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
