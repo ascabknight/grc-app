727cf44685a9994c21f0253404882992573795df
Mauricio Aspiazo <mau@tauruseer.com>
2020-09-10 11:14:14 -0500

    Fifty-fifty breakpoint fixed

--- src/components/content_50/index.js
+++ src/components/content_50/index.js
@@ -6,12 +6,12 @@ import styles from './content_50.scss';
 
 export const Content_50 = ({ imgUrl, title, headline, ctaUrl, ctaText }) => (
   <>
-    <Col className="center-content-column">
+    <Col sm className="center-content-column">
       <Row>
         <Image src={imgUrl} />
       </Row>
     </Col>
-    <Col className="flex-column-center">
+    <Col className="flex-column-center mt-3">
       <Row>
         <Title className="display-5">{title}</Title>
         <Headline>{headline}</Headline>
